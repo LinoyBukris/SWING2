6c161d4ab4b2bf77330aed65e4d7ea9b2f9fc485
LinoyBukris <020linoy020@gmail.com>
2024-07-21 14:09:46 +0300

    Add files via upload

---
+++ Programming__02/src/main/java/org/example/Programming__02/src/main/java/org/example/Window.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Window extends JFrame {
+
+    public static final int WINDOW_WIDTH =600;
+    public static final int WINDOW_HEIGHT =600;
+
+    public Window(){
+        this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        this.setResizable(false);
+        this.setLayout(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+        MainScene mainScene = new MainScene();
+        this.add(mainScene);
+
+        MenuPanel menuPanel = new MenuPanel();
+        this.add(menuPanel);
+
+        mainScene.setFocusable(true);
+        mainScene.requestFocusInWindow();
+
+    }
+
+
+
+
+}
